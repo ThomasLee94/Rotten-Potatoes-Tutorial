3d751155718737877cc852d357114ef90d64b4ad
ThomasLee94 <thomaslee9474@gmail.com>
2018-09-17 22:09:26 -0700

    users can see single reviews

--- app.js
+++ app.js
@@ -53,22 +53,23 @@ app.get("/reviews/new", (req, res) => {
 
 })
 
-//Create
-app.post("/reviews", (req, res) => {
-    Review.create(req.body).then((review) => {
-        console.log(review);
-        res.redirect("/");
-    }).catch((err) => {
-        console.log(err.message);
-    })
+// CREATE
+app.post('/reviews', (req, res) => {
+  Review.create(req.body).then((review) => {
+    console.log(review)
+    res.redirect(`/reviews/${review._id}`) // Redirect to reviews/:id
+  }).catch((err) => {
+    console.log(err.message)
+  })
 })
 
-app.get("/reviews/:id", (req, res) => {
-    Review.findById(req.params.id).then((review) => {
-        res.render("reviews-show", {review: review})
-    }).catch((err) => {
-        console.log(err.message);
-    })
+// SHOW
+app.get('/reviews/:id', (req, res) => {
+  Review.findById(req.params.id).then((review) => {
+    res.render('reviews-show', { review: review })
+  }).catch((err) => {
+    console.log(err.message);
+  })
 })
 
 app.engine("handlebars", exphbs({defaultLayout: "main"}));
