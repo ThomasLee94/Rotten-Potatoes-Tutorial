818f7dd72642aec304e2b5b7f75cc50db7f36b82
Thomas Lee <thomaslee9474@gmail.com>
2018-09-30 17:30:56 -0700

    improved movies-show template + trailer

--- controllers/movies.js
+++ controllers/movies.js
@@ -3,6 +3,7 @@ var MovieDb = require("moviedb-promise")
 var moviedb = new MovieDb("b52d24adafbce5b2208933ddd6c0b636")
 
 module.exports =  function(app) {
+    // Show all movies (index route)
     app.get("/", (req, res) => {
         moviedb.miscNowPlayingMovies().then(response => {
             res.render("movies-index", {movies: response.results})
@@ -10,20 +11,23 @@ module.exports =  function(app) {
         }).catch(console.error)
     })
 
-    app.get('/movies', (req, res) => {
-        res.render('movies-index');
-    })
-
-    app.get("/movies/:id", (req, res) => {
-        moviedb.movieInfo({id: req.params.id}).then(movie => {
-            console.log(movie)
-            res.render("movies-view", {movie: movie})
+    // Show a single movie + trailer
+    app.get('/movies/:id', (req, res) => {
+        moviedb.movieInfo({ id: req.params.id }).then(movie => {
+          if (movie.video) {
+            moviedb.movieVideos({ id: req.params.id }).then(videos => {
+              movie.trailer_youtube_id = videos.results[0].key
+              renderTemplate(movie)
+            })
+          } else {
+            renderTemplate(movie)
+          }
+      
+          function renderTemplate(movie)  {
+            res.render('movies-show', { movie: movie });
+          }
+      
         }).catch(console.error)
-    })
-
-    app.get("/movie/:id", (req, res) => {
-        moviedb.movieInfo({id: req.params.id}).then(movie => {
-            res.render("movies-show", {movie: movie});
-        })
-    }).catch(console.error)
+      })
+    
 }
